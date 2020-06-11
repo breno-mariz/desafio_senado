9de3fd9ba7c7f563da53cde64a214491da955629
Breno Mariz <mrzbeo@gmail.com>
2020-06-11 19:11:41 -0300

    limpando projeto inicial

--- parliber/src/App.js
+++ parliber/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><h3 className="App-logo">Ma boy</h3></p>
       </header>
+      <h3>Ma gal</h3>
     </div>
   );
 }
